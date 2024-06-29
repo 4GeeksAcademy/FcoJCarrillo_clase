28020c2684feabdbf37291a9cad3dd206b848acb
Francisco Javier Carrillo Carrillo <133151654+FcoJCarrillo@users.noreply.github.com>
2024-06-29 10:25:28 +0000

    Plantilla

---
+++ src/front/js/pages/Error404.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+export const Error404 = () =>{
+    return(
+        <div className="container justify-content-center">
+            <h1 className="text-danger">Página no encontrada</h1>
+        </div>
+    )
+}
