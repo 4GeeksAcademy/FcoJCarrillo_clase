a9f792b04a125936773fce526dbeeb30a5a4cb42
Francisco Javier Carrillo Carrillo <133151654+FcoJCarrillo@users.noreply.github.com>
2024-07-27 17:55:01 +0000

    Instagram Diagrama

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,7 +1,7 @@
 import os
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
-from .models import db, Users, Media, Post
+from .models import db, Users, Media, Post, Comment
 
 
 def setup_admin(app):
@@ -13,3 +13,4 @@ def setup_admin(app):
     # admin.add_view(ModelView(YourModelName, db.session)) You can duplicate that line to add mew models
     admin.add_view(ModelView(Media, db.session))
     admin.add_view(ModelView(Post, db.session))
+    admin.add_view(ModelView(Comment, db.session))
--- src/api/models.py
+++ src/api/models.py
@@ -12,8 +12,8 @@ class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(120), unique=True, nullable=False)
     firstname = db.Column(db.String(80), unique=False, nullable=False)
-    lastname = db.Column(db.Boolean(), unique=False, nullable=False)
-    email = db.Column(db.Boolean(), unique=False, nullable=False)
+    lastname = db.Column(db.String(80), unique=False, nullable=False)
+    email = db.Column(db.String(80), unique=False, nullable=False)
 
     def __repr__(self):
         return f'<User {self.email}>'
@@ -26,10 +26,13 @@ class Users(db.Model):
                 "lastname": self.lastname,
                 "email": self.email}
 
+
 class Post(db.Model):
     __tablename__ = 'post'
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    user_to = db.relationship('Users', foreign_keys=[user_id],
+                                backref=db.backref('users_to', lazy='select'))
 
     def __repr__(self):
         return f'<Post {self.id} - {self.user_id}>'
@@ -38,10 +41,11 @@ class Post(db.Model):
         return{"id": self.id,
                 "user_id": self.user_id}
 
+
 class Media(db.Model):
     __tablename__ = 'media'
     id = db.Column(db.Integer, primary_key=True)
-    #media_type = db.Column(db.Enum('video', 'image', name='media_type'), nullable=True)
+    media_type = db.Column(db.Enum('video', 'image', name='media_type'), unique=False, nullable=True)
     url = db.Column(db.String)
     post_id = db.Column(db.Integer, db.ForeignKey('post.id'))
     poster_id = db.relationship('Post', foreign_keys=[post_id],
@@ -56,4 +60,26 @@ class Media(db.Model):
         return{"id": self.id,
                 "media_type": self.media_type,
                 "url": self.url,
+                "post_id": self.post_id}
+
+
+class Comment(db.Model):
+    __tablename__ = 'comment'
+    id = db.Column(db.Integer, primary_key=True)
+    comment_text = db.Column(db.String)
+    author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'))
+    author_to = db.relationship('Users', foreign_keys=[author_id],
+                                backref=db.backref('author_to', lazy='select'))
+    post_to = db.relationship('Post', foreign_keys=[post_id],
+                                backref=db.backref('post_to', lazy='select'))
+
+    def __repr__(self):
+        return f'<Comment {self.id} - {self.comment_text} - {self.author_id} - {self.post_id}>'
+    
+
+    def serializa(self):
+        return{"id": self.id,
+                "comment_text": self.comment_text,
+                "author_id": self.author_id,
                 "post_id": self.post_id}
